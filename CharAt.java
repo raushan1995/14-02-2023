04a2c85c3e14a9b5df9ca3d28bdc36c8c528a6d4
Raushan Kumar <priyanshu1996r@gmail.com>
2023-02-14 23:02:18 +0530

    Add files via upload

---
+++ CharAt.java
@@ -0,0 +1,10 @@
+public class CharAtExample {  
+    public static void main(String[] args) {  
+    String str = "Welcome to Javatpoint portal";      
+    int strLength = str.length();      
+      
+    System.out.println("Character at 0 index is: "+ str.charAt(0));      
+     
+    System.out.println("Character at last index is: "+ str.charAt(strLength-1));      
+    }  
+}  
